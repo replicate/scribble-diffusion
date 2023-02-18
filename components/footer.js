82fe9fc38fe782948f9434238f619747b6866b08
Ben Firshman <ben@firshman.com>
2023-02-18 11:57:44 -0800

    Add github repo to developer bit

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,9 @@ export default function Footer({ events }) {
     <footer className="w-full my-8">
       <div className="text-center lil-text mt-8">
         <div className="inline-block py-3 px-4 border bg-gray-200 rounded-lg">
-          🍿 Want to build an app like this? Check out the{" "}
+          🍿 Want to build an app like this? <Link
+          href="https://github.com/replicate/scribble-diffusion"
+          target="_blank">Fork it on GitHub</Link> or check out the{" "}
           <Link href="https://youtu.be/6z07OdbrWOs" target="_blank">
             video tutorial
           </Link>
@@ -36,13 +38,8 @@ export default function Footer({ events }) {
         <Link href="https://upload.io" target="_blank">
           Upload
         </Link>
-        .{" "}
-        <Link
-          href="https://github.com/replicate/scribble-diffusion"
-          target="_blank">
+        .
         
-          Source is on GitHub
-        </Link>.
       </div>
     </footer>
   );
