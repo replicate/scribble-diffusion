b31d12de25a101eb0267cf7f7b551f07cca10f29
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-21 22:29:02 +0800

    Add comments to the code
    I included comments to the line of code so that other developers  can follow along through reading comments :)

--- components/canvas.js
+++ components/canvas.js
@@ -1,48 +1,66 @@
+// Importing necessary modules
 import * as React from "react";
 import { useEffect } from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
-
 import { Undo as UndoIcon, Trash as TrashIcon } from "lucide-react";
 
+// Defining the component named Canvas which takes some props
 export default function Canvas({
   startingPaths,
   onScribble,
   scribbleExists,
   setScribbleExists,
 }) {
+
+  // Creating a reference to the canvas element
   const canvasRef = React.useRef(null);
 
+  // Use effect hook to load the starting paths when the component mounts
   useEffect(() => {
     loadStartingPaths();
   }, []);
 
+  // Function to load the starting paths
   async function loadStartingPaths() {
+    // Wait until the starting paths are loaded
     await canvasRef.current.loadPaths(startingPaths);
+    // Set scribble exists to true
     setScribbleExists(true);
+    // Call the onChange function
     onChange();
   }
 
+  // Function that is called whenever a change is made in the canvas
   const onChange = async () => {
+    // Export the paths as JSON string and store it in local storage
     const paths = await canvasRef.current.exportPaths();
     localStorage.setItem("paths", JSON.stringify(paths, null, 2));
 
+    // If there are no paths, return from the function
     if (!paths.length) return;
 
+    // Set scribble exists to true
     setScribbleExists(true);
 
+    // Export the image as png and pass the data to the onScribble callback
     const data = await canvasRef.current.exportImage("png");
     onScribble(data);
   };
 
+  // Function to undo the last action in the canvas
   const undo = () => {
     canvasRef.current.undo();
   };
 
+  // Function to clear the canvas
   const reset = () => {
+    // Set scribble exists to false
     setScribbleExists(false);
+    // Reset the canvas
     canvasRef.current.resetCanvas();
   };
 
+  // Return JSX with the canvas, buttons and a placeholder if there is no scribble
   return (
     <div className="relative">
       {scribbleExists || (
