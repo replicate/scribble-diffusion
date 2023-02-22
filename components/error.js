8a07011a70e32b8e02bb4465b01dc7e30a312a3d
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-22 08:40:31 +0800

    Update Footer component to use Tailwind CSS class names
    This commit updates the Footer component to use class names from the Tailwind CSS framework, which helps keep our code consistent and reduces the likelihood of naming conflicts. The bold class has been replaced with the font-bold class, the equivalent class name in Tailwind. The pb-5 class has been added to create padding at the bottom of the error message.

--- components/error.js
+++ components/error.js
@@ -1,9 +1,21 @@
+/**
+ * The Footer component displays an error message if one is provided
+ *
+ * @param {Object} props - The props object containing the `error` prop
+ * @returns {JSX.Element|null} The rendered component, or null if no error is provided
+ */
 export default function Footer({ error }) {
-  if (!error) return null;
+  // If no error is provided, do not render anything
+  if (!error) {
+    return null;
+  }
 
+  // Otherwise, display the error message within a container div
   return (
     <div className="mx-auto w-full">
-      {error && <p className="bold text-red-500 pb-5">{error}</p>}
+      {error && (
+        <p className="font-bold text-red-500 pb-5">{error}</p>
+      )}
     </div>
   );
 }
