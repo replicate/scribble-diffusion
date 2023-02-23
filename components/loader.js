41d5c3427817201244a40f633dd77c8cb21b92a9
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-23 21:33:30 +0800

    Add Comments to loader component

--- components/loader.js
+++ components/loader.js
@@ -1,8 +1,12 @@
+// Import the PulseLoader component from the react-spinners package
 import PulseLoader from "react-spinners/PulseLoader";
 
+// Define a functional component called Loader
 export default function Loader() {
   return (
+    // A container element that wraps the PulseLoader component
     <div>
+      {/* The PulseLoader component with props to set its size, margin, and opacity */}
       <PulseLoader size={12} margin={4} className="opacity-40" />
     </div>
   );
