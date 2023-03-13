5a53ec36a037e7c137e195ea6c067e7d97198ee4
Zeke Sikelianos <zeke@sikelianos.com>
2023-03-13 11:50:56 -0700

    use replicate npm client

--- pages/api/predictions/[id].js
+++ pages/api/predictions/[id].js
@@ -1,19 +1,13 @@
-const API_HOST = process.env.REPLICATE_API_HOST || "https://api.replicate.com";
+import replicate from "replicate";
 
 export default async function handler(req, res) {
-  const response = await fetch(`${API_HOST}/v1/predictions/${req.query.id}`, {
-    headers: {
-      Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-  });
-  if (response.status !== 200) {
-    let error = await response.json();
+  const prediction = await replicate.prediction(req.query.id).load();
+
+  if (prediction.error) {
     res.statusCode = 500;
-    res.end(JSON.stringify({ detail: error.detail }));
+    res.end(JSON.stringify({ detail: prediction.error }));
     return;
   }
 
-  const prediction = await response.json();
   res.end(JSON.stringify(prediction));
 }
